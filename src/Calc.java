caa3430f3a3523cafd1c95203297933aeaff4050
AlinaCumber <alinavevel@gmail.com>
2021-08-02 20:10:51 +0300

    Style change

--- src/Calc.java
+++ src/Calc.java
@@ -7,11 +7,11 @@ public class Calc {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
 
-    public int[] GetNumAr(){
+    public int[] GetNumAr() {
         return numAr;
     }
 
-    public String[] GetNumRom(){
+    public String[] GetNumRom() {
         return numRom;
     }
 
@@ -25,13 +25,13 @@ public class Calc {
             System.out.println("Введите арабские или римские цифры: ");
             System.out.println("Для выхода наберите exit");
             String line = scanner.nextLine();
-            if(line.equals("exit")){
+            if (line.equals("exit")) {
                 System.out.println("До свидания");
                 break;
             }
             String[] splitLine = line.split(" ");
             if (splitLine.length != 3) {
-                                System.out.println("\033[H\033[2J");
+                System.out.println("\033[H\033[2J");
                 System.out.flush();
                 throw new IllegalArgumentException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
 
@@ -40,49 +40,46 @@ public class Calc {
             String b = splitLine[2];
             String op = splitLine[1];
 
-            if(Arrays.asList(numRom).contains(a) && Arrays.asList(numRom).contains(b)){
+            if (Arrays.asList(numRom).contains(a) && Arrays.asList(numRom).contains(b)) {
                 int number = ConvertNumbers.RomanToArab(a);
                 int number2 = ConvertNumbers.RomanToArab(b);
-                int result = MathCalc.Calc(number,number2, op);
-                if(result < 0){
+                int result = MathCalc.Calc(number, number2, op);
+                if (result < 0) {
 
-                        throw new Exception("т.к. в римской системе нет отрицательных чисел");
+                    throw new Exception("т.к. в римской системе нет отрицательных чисел");
 
                 }
-                if(result > 10){
+                if (result > 10) {
                     System.out.println(ConvertNumbers.ArabToRoman(result));
                     System.out.println("\033[H\033[2J");
                     System.out.flush();
 
-                }
-                else if (result == 0 ){
+                } else if (result == 0) {
 
                     System.out.println("N");
                     System.out.println("\033[H\033[2J");
                     System.out.flush();
 
-                }
-                else{
+                } else {
                     String resulta = numRom[result - 1];
                     System.out.println(resulta);
                     System.out.println("\033[H\033[2J");
                     System.out.flush();
                 }
 
-            }
-            else{
-                int number = VerifyData.tryParseInt(a, -11);
-                int number2 = VerifyData.tryParseInt(b, -11);
+            } else {
+                int number = VerifyData.tryParseInt(a, -1);
+                int numberTwo = VerifyData.tryParseInt(b, -1);
 
-                if (number == -11 || number2 == -11 || number > 10 || number2 > 10){
-                    throw new Exception("т.к. используются одновременно разные системы счисления");
+                if (number < 0 || numberTwo < 0 || number > 10 || numberTwo > 10) {
+                    throw new Exception("Только одна система исчисления. и только от 1 до 10");
                 }
 
 
                 for (int i = 0; i < numAr.length; i++) {
                     for (int j = 0; j < numAr.length; j++) {
-                        if (number == numAr[i] && number2 == numAr[j]) {
-                            int result = MathCalc.Calc(number, number2, op );
+                        if (number == numAr[i] && numberTwo == numAr[j]) {
+                            int result = MathCalc.Calc(number, numberTwo, op);
                             System.out.println(result);
                             System.out.println("\033[H\033[2J");
                             System.out.flush();
